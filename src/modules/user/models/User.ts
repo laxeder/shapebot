8e3cc191a6c03af708879442d0c382b1c97f504f
laxeder <laxederbr@gmail.com>
2023-08-27 12:14:48 -0300

    remove: last send menu

--- src/modules/user/models/User.ts
+++ src/modules/user/models/User.ts
@@ -3,7 +3,6 @@ import DataModel from "@modules/database/models/DataModel";
 export default class User extends DataModel {
   public botId: string = "";
   public id: string = "";
-  public lastSendMenu: number = 0;
 
   constructor(data: any = {}) {
     super();
