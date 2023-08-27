bb4cb9a4e0bd20f2c900b1de1bea101b6543ada1
laxeder <laxederbr@gmail.com>
2023-08-26 21:36:50 -0300

    add: text utils

---
+++ src/utils/TextUtils.ts
@@ -0,0 +1,75 @@
+export default class TextUtils {
+  constructor(public text: string = "") {}
+
+  public clean(text: string = ""): this {
+    this.text = text;
+
+    return this;
+  }
+
+  public getText(): string {
+    return this.text;
+  }
+
+  public add(text: string): this {
+    this.text += `${text}`;
+
+    return this;
+  }
+
+  public addLine(text: string = ""): this {
+    this.text += `\n${text}`;
+
+    return this;
+  }
+
+  public static bold(text: string): string {
+    return `*${text}*`;
+  }
+
+  public static italic(text: string): string {
+    return `_${text}_`;
+  }
+
+  public static lineDecorator(): string {
+    return `\n▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔`;
+  }
+
+  public static getMoneyValue(value: number): string {
+    let str = String(value).replace(".", ",");
+
+    if (!str.includes(",")) str = `${str},00`;
+
+    str = str
+      .split(",")
+      .map((s, i) => {
+        if (i == 0) return s;
+        if (s.length < 1) return "00";
+        if (s.length < 2) return `${s}0`;
+        return s;
+      })
+      .join(",");
+
+    return str;
+  }
+
+  public static generateMoneyValue(value: string): number {
+    const money = value
+      .split(",")
+      .slice(0, 2)
+      .map((v) => v.replace(/\D+/g, "") || "0")
+      .join(".");
+
+    return Number(money || 0);
+  }
+
+  public static generateOptions(list: string[]): string {
+    let text: string = "";
+
+    list.forEach((item, index) => {
+      text += `\n[${String(index + 1).padStart(2, "0")}] ${item}`;
+    });
+
+    return text.trim();
+  }
+}
