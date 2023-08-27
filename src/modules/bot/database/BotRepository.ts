734a351850011625cc8a20e1f66e1d5e54cdf3e8
laxeder <laxederbr@gmail.com>
2023-08-26 21:57:29 -0300

    add: bot repository doc

--- src/modules/bot/database/BotRepository.ts
+++ src/modules/bot/database/BotRepository.ts
@@ -10,6 +10,11 @@ import DateUtils from "@utils/Date";
 export default class BotRepository implements DataRepository<Bot> {
   constructor(private db: Database) {}
 
+  /**
+   * Cria um novo bot.
+   * @param bot - O bot a ser criado.
+   * @throws ClientError se o ID do bot não estiver definido ou se um bot com o mesmo ID já existir.
+   */
   public async create(bot: Bot): Promise<void> {
     if (!bot.id) {
       throw new ClientError("Bot id not declared", "Não foi possível salvar os dados do bot");
@@ -28,6 +33,11 @@ export default class BotRepository implements DataRepository<Bot> {
     await this.db.save("/bots", bot);
   }
 
+  /**
+   * Atualiza os dados de um bot existente.
+   * @param bot - Os campos atualizados do bot.
+   * @throws ClientError se o ID do bot não estiver definido ou se o bot não estiver habilitado.
+   */
   public async update(bot: Partial<Bot>): Promise<void> {
     if (!bot.id) {
       throw new ClientError("Bot id not declared", "Não foi possível atualizar os dados do bot");
@@ -46,6 +56,12 @@ export default class BotRepository implements DataRepository<Bot> {
     await this.db.save("/bots", data);
   }
 
+  /**
+   * Lê os dados de um bot.
+   * @param bot - O bot cujos dados serão lidos.
+   * @returns O bot lido.
+   * @throws ClientError se o ID do bot não estiver definido
+   */
   public async read(bot: Bot): Promise<Bot> {
     if (!bot.id) {
       throw new ClientError("Bot id not declared", "Não foi possível ler os dados do bot");
@@ -56,6 +72,11 @@ export default class BotRepository implements DataRepository<Bot> {
     return new Bot(data);
   }
 
+  /**
+   * Exclui um bot.
+   * @param bot - O bot a ser excluído.
+   * @throws ClientError se o bot não estiver habilitado ou em caso de erro.
+   */
   public async delete(bot: Bot): Promise<void> {
     const data = await this.read(bot);
 
@@ -69,6 +90,11 @@ export default class BotRepository implements DataRepository<Bot> {
     await this.db.save("/bots", data);
   }
 
+  /**
+   * Restaura um bot previamente excluído.
+   * @param bot - O bot a ser restaurado.
+   * @throws ClientError se o bot não estiver desabilitado ou em caso de erro.
+   */
   public async restore(bot: Bot): Promise<void> {
     const data = await this.read(bot);
 
@@ -82,6 +108,10 @@ export default class BotRepository implements DataRepository<Bot> {
     await this.db.save("/bots", data);
   }
 
+  /**
+   * Obtém uma lista de todos os bots habilitados.
+   * @returns Uma lista de bots habilitados.
+   */
   public async findAll(): Promise<Bot[]> {
     const list = await this.db.findAll("/bots");
 
