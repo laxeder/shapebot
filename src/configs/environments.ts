3ea94097b7a4eb8b97e2059248847ce82304b2a5
laxeder <laxederbr@gmail.com>
2023-08-27 11:59:38 -0300

    add: environments config

---
+++ src/configs/environments.ts
@@ -0,0 +1,67 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+/**
+ * @param key chave
+ * @returns retorna um valor dos enviroments
+ */
+export function getEnv(key: string): string {
+  return String(process.env[key]);
+}
+
+/**
+ * * Define um valor nos enviroments
+ * @param key chave
+ * @param value valor
+ */
+export function setEnv(key: string, value: string) {
+  return (process.env[key] = value);
+}
+
+/**
+ * @returns Retorna o ambiente
+ */
+export function getEnvironment(): string {
+  return process.env.NODE_ENV?.trim() || "development";
+}
+
+/**
+ * @returns Retorna a host do servidor
+ */
+export function getHost(): string {
+  const HOST = getEnvironment() == "production" ? process.env.HOST : process.env.DEV_HOST;
+  return HOST ? `${HOST}` : `http://localhost:${getPort()}`;
+}
+
+/**
+ * @returns Retorna a porta do servidor
+ */
+export function getPort(): string {
+  const PORT = getEnvironment() == "production" ? process.env.PORT : process.env.DEV_PORT;
+  return PORT || "3000";
+}
+
+/**
+ * @returns Retorna o tempo de desligamento gracioso
+ */
+export function getShutdown(): string {
+  const SHUTDOWN = getEnvironment() == "production" ? process.env.SHUTDOWN : process.env.DEV_SHUTDOWN;
+  return SHUTDOWN || "1s";
+}
+
+/**
+ * @returns Retorna o IP do RedisDB
+ */
+export function getRedisIP(): string {
+  const RedisIP = getEnvironment() == "production" ? process.env.REDIS_IP : process.env.REDIS_IP_DEV;
+  return RedisIP || "";
+}
+
+/**
+ * @returns Retorna o PORT do RedisDB
+ */
+export function getRedisPORT(): string {
+  const RedisPORT = getEnvironment() == "production" ? process.env.REDIS_PORT : process.env.REDIS_PORT_DEV;
+  return RedisPORT || "";
+}
