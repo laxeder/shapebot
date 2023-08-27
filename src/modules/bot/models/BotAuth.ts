65c4096a919af0d326531432d68ed1aced0f1abd
laxeder <laxederbr@gmail.com>
2023-08-26 21:34:07 -0300

    add: bot auth

---
+++ src/modules/bot/models/BotAuth.ts
@@ -0,0 +1,64 @@
+import { IAuth } from "rompot";
+
+import Database from "@modules/database/interfaces/Database";
+
+import Logger from "@shared/Logger";
+
+import { len } from "@utils/JSON";
+
+export default class BotAuth implements IAuth {
+  public botId: string;
+
+  constructor(botId: string, private db: Database) {
+    this.botId = botId;
+  }
+
+  public genKey(botId?: string, authKey?: string): string {
+    let key = `auths`;
+
+    if (!!botId) key += `/${botId}`;
+    if (!!authKey) key += `/${authKey.replace(/[/:]/g, ".")}`;
+
+    return key;
+  }
+
+  public async get(key: string): Promise<any> {
+    try {
+      const data = await this.db.findAll(this.genKey(this.botId, key));
+
+      if (!Array.isArray(data) && len(data) == 0) return null;
+
+      return data;
+    } catch (err) {
+      Logger.error(err, `Erro ao obter sessão (${this.botId}/${key})`);
+
+      return null;
+    }
+  }
+
+  public async set(key: string, data: any): Promise<void> {
+    try {
+      await this.db.save(this.genKey(this.botId, key), data);
+    } catch (err) {
+      Logger.error(err, `Erro ao salvar sessão (${this.botId}:${key})`);
+    }
+  }
+
+  public async remove(key: string): Promise<void> {
+    try {
+      await this.db.remove(this.genKey(this.botId), key);
+    } catch (err) {
+      Logger.error(err, `Erro ao remover sessão (${this.botId}/${key})`);
+    }
+  }
+
+  public async listAll(key?: string | undefined): Promise<string[]> {
+    try {
+      return Object.keys(await this.db.findAll(this.genKey(this.botId, key)));
+    } catch (err) {
+      Logger.error(err, `Erro ao listar sessão (${this.botId}:${key})`);
+    }
+
+    return [];
+  }
+}
