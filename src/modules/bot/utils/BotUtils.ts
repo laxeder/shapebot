89b5a6f0d0704d8f327f88b108f564e99e197717
laxeder <laxederbr@gmail.com>
2023-08-26 22:03:48 -0300

    add: bot utils doc

--- src/modules/bot/utils/BotUtils.ts
+++ src/modules/bot/utils/BotUtils.ts
@@ -3,7 +3,16 @@ import { IMessage } from "rompot-base";
 
 import TextUtils from "@utils/TextUtils";
 
+/**
+ * Uma classe utilitária que contém métodos para interações com usuários através de mensagens.
+ */
 export default class BotUtils {
+  /**
+   * Aguarda até que um usuário envie uma mensagem em um determinado chat.
+   * @param botId - O ID do bot.
+   * @param chatId - O ID do chat.
+   * @returns A mensagem enviada pelo usuário.
+   */
   public static async awaitUserSendMessage(botId: string, chatId: string): Promise<IMessage> {
     const client = ClientUtils.getClient(botId);
 
@@ -27,6 +36,13 @@ export default class BotUtils {
     return msg;
   }
 
+  /**
+   * Aguarda até que um usuário envie uma confirmação ("sim", "não" ou similar).
+   * @param botId - O ID do bot.
+   * @param chatId - O ID do chat.
+   * @param text - O texto opcional a ser exibido para solicitar a confirmação.
+   * @returns `true` se o usuário confirmou, `false` se o usuário negou e `null` se a confirmação foi cancelada.
+   */
   public static async awaitUserSendConfirmation(botId: string, chatId: string, text?: string): Promise<boolean | null> {
     const msg = await BotUtils.awaitUserSendMessage(botId, chatId);
 
@@ -45,6 +61,13 @@ export default class BotUtils {
     return await BotUtils.awaitUserSendConfirmation(botId, chatId, text);
   }
 
+  /**
+   * Aguarda até que um usuário selecione uma opção de uma lista.
+   * @param botId - O ID do bot.
+   * @param chatId - O ID do chat.
+   * @param list - A lista de opções para escolher.
+   * @returns O índice da opção selecionada ou `null` se cancelado.
+   */
   public static async awaitUserSendOption(botId: string, chatId: string, list: Array<string | number>): Promise<number | null> {
     const msg = await BotUtils.awaitUserSendMessage(botId, chatId);
 
@@ -80,6 +103,12 @@ export default class BotUtils {
     return Number(option.toFixed(0)) - 1;
   }
 
+  /**
+   * Aguarda até que um usuário envie uma mensagem de texto em um chat.
+   * @param botId - O ID do bot.
+   * @param chatId - O ID do chat.
+   * @returns A mensagem de texto enviada pelo usuário ou `null` se cancelado.
+   */
   public static async awaitUserSendText(botId: string, chatId: string): Promise<string | null> {
     const msg = await BotUtils.awaitUserSendMessage(botId, chatId);
 
@@ -97,6 +126,13 @@ export default class BotUtils {
     return msg.text.trim();
   }
 
+  /**
+   * Aguarda até que um usuário envie um número.
+   * @param botId - O ID do bot.
+   * @param chatId - O ID do chat.
+   * @param allNumbers - Se deve aceitar qualquer sequência numérica ou apenas números maiores que zero.
+   * @returns O número enviado pelo usuário ou `null` se cancelado.
+   */
   public static async awaitUserSendNumber(botId: string, chatId: string, allNumbers: boolean = false): Promise<number | null> {
     const msg = await BotUtils.awaitUserSendMessage(botId, chatId);
 
@@ -116,6 +152,12 @@ export default class BotUtils {
     return number;
   }
 
+  /**
+   * Aguarda até que um usuário envie um número de telefone.
+   * @param botId - O ID do bot.
+   * @param chatId - O ID do chat.
+   * @returns O número de telefone enviado pelo usuário ou `null` se cancelado.
+   */
   public static async awaitUserSendPhoneNumber(botId: string, chatId: string): Promise<string | null> {
     const msg = await BotUtils.awaitUserSendMessage(botId, chatId);
 
@@ -135,6 +177,12 @@ export default class BotUtils {
     return userId;
   }
 
+  /**
+   * Aguarda até que um usuário envie um valor monetário.
+   * @param botId - O ID do bot.
+   * @param chatId - O ID do chat.
+   * @returns O valor monetário enviado pelo usuário ou `null` se cancelado.
+   */
   public static async awaitUserSendMoneyValue(botId: string, chatId: string): Promise<number | null> {
     const msg = await BotUtils.awaitUserSendMessage(botId, chatId);
 
