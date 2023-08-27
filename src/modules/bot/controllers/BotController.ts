4437610edb2f2f42b02bc2c921c897f51d46d555
laxeder <laxederbr@gmail.com>
2023-08-26 21:53:17 -0300

    add: doc

--- src/modules/bot/controllers/BotController.ts
+++ src/modules/bot/controllers/BotController.ts
@@ -16,17 +16,33 @@ import Bot from "@modules/bot/models/Bot";
 export default class BotController {
   constructor(private repo: DataRepository<Bot>) {}
 
+  /**
+   * Cria um novo bot.
+   * @param bot - O bot a ser criado.
+   * @throws ClientError se o bot já estiver habilitado.
+   */
   public async createBot(bot: Bot): Promise<void> {
     await this.repo.create(bot);
   }
 
+  /**
+   * Atualiza um bot existente.
+   * @param bot - Os campos atualizados do bot.
+   * @throws ClientError se o bot não estiver habilitado.
+   */
   public async updateBot(bot: Partial<Bot>): Promise<void> {
     await this.repo.update(bot);
   }
 
+  /**
+   * Obtém um bot pelo seu ID
+   * @param id - O ID do bot a ser obtido.
+   * @returns O bot obtido.
+   */
   public async getBotById(id: string): Promise<Bot> {
     const bot = await this.repo.read(new Bot({ id }));
 
+    // Adiciona o ID do bot à lista de administradores, se ainda não estiver presente
     bot.id = id;
 
     if (!bot.admins.includes(bot.id)) {
@@ -36,32 +52,51 @@ export default class BotController {
     return bot;
   }
 
+  /**
+   * Lista todos os bots habilitados
+   * @returns Uma lista de bots habilitados.
+   */
   public async listAllBots(): Promise<Bot[]> {
     const allBots = await this.repo.findAll();
 
-    const bots: Bot[] = [];
+    const enabledBots: Bot[] = [];
 
     for (const bot of allBots) {
-      if (bot.status != DataStatus.Enabled) continue;
+      // Verifica se o bot está habilitado
+      if (bot.status !== DataStatus.Enabled) continue;
 
+      // Adiciona o ID do bot à lista de administradores, se necessário
       if (!bot.admins.includes(bot.id)) {
         bot.admins.push(bot.id);
       }
 
-      bots.push(bot);
+      enabledBots.push(bot);
     }
 
-    return bots;
+    return enabledBots;
   }
 
+  /**
+   * Exclui um bot pelo seu ID.
+   * @param id - O ID do bot a ser excluído.
+   */
   public async deleteBotById(id: string): Promise<void> {
     await this.repo.delete(new Bot({ id }));
   }
 
+  /**
+   * Restaura um bot pelo seu ID.
+   * @param id - O ID do bot a ser restaurado.
+   */
   public async restoreBotById(id: string): Promise<void> {
     await this.repo.restore(new Bot({ id }));
   }
 
+  /**
+   * Inicia um bot.
+   * @param botId - O ID do bot a ser iniciado.
+   * @param db - A instância da base de dados.
+   */
   public static async startBot(botId: string, db: Database) {
     //! ===== Configurando bot ===== !\\
 
