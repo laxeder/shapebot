c938435453df161c86ce8f09c61c88169f05f34f
laxeder <laxederbr@gmail.com>
2023-08-28 20:18:02 -0300

    fix: command running

--- src/modules/command/utils/CommandDataUtils.ts
+++ src/modules/command/utils/CommandDataUtils.ts
@@ -4,10 +4,10 @@ import ICommandData from "@modules/command/interfaces/ICommandData";
 
 export default class CommandDataUtils {
   public static generateEmpty<T extends Record<string | number | symbol, any>>(data: T): ICommandData & T {
-    return { ...{ lastMessage: new Message("", ""), currentFunctionIndex: 0 }, ...data };
+    return { ...{ lastMessage: new Message("", ""), currentFunctionIndex: 0, running: false }, ...data };
   }
 
   public static generate<T extends Record<string | number | symbol, any>>(lastMessage: IMessage, currentFunctionIndex: number, data: T): ICommandData & T {
-    return { lastMessage, currentFunctionIndex, ...data };
+    return { lastMessage, currentFunctionIndex, running: false, ...data };
   }
 }
