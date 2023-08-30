eb353bd406f0dfd497e99a1f041641d3391968da
laxeder <laxederbr@gmail.com>
2023-08-30 16:22:30 -0300

    fix: commands path

--- src/modules/bot/controllers/BotController.ts
+++ src/modules/bot/controllers/BotController.ts
@@ -158,7 +158,7 @@ export default class BotController {
 
     client.setCommandController(commandController);
 
-    const commands = await CommandController.readCommands(`${__dirname}/src/commands`);
+    const commands = await CommandController.readCommands(`${__dirname}/../../../commands`);
 
     client.setCommands(commands);
 
